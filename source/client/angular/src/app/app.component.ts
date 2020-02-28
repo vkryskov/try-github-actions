bf6c904c730f31c88c3b3da88834a8b1d23e2e6c
Kryskov Vitalii <vitalii.kryskov@avenga.com>
2020-02-28 16:51:10 +0200

    fix

--- source/client/angular/src/app/app.component.ts
+++ source/client/angular/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "y";
-
-  constructor() {
-    this.title = {};
-  }
+  title = 'y'
 }
